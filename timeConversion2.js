39ba6b8d79869ad3d749eeffa580eae5d05f3456
Daniel Viana <vianadaniel@hotmail.com>
2021-01-19 11:59:08 -0300

    segundo video

---
+++ timeConversion2.js
@@ -0,0 +1,22 @@
+let time = "12:01:00PM"
+let time2 = "06:40:03AM"
+
+function timeConversion(time) {
+  let arr = time.slice(0, 8).split(":")
+
+  if (time[8] == "P") {
+    if (arr[0] == "12") {
+      arr[0] = "12"
+    } else {
+      arr[0] = Number(arr[0]) + 12
+    }
+  } else if (time[8] == "A") {
+    if (arr[0] == "12") {
+      arr[0] = "00"
+    }
+  }
+  arr = arr.join(":")
+  console.log(arr)
+}
+
+timeConversion(time)
