4172934d7eedce8582c18f47ed78016d3bb7be21
Daniel Viana <vianadaniel@hotmail.com>
2021-01-24 12:31:58 -0300

    fim

---
+++ insertNodePosition.js
@@ -0,0 +1,25 @@
+function insertNodeAtTail(head, data, position) {
+  let counter = 1
+  let newnode = new SinglyLinkedListNode(data)
+  let node = head
+
+  if (position == 0) {
+    node = new SinglyLinkedListNode(data)
+    node.next = head
+  }
+
+  while (counter != position) {
+    node = node.next
+    counter += 1
+  }
+
+  newnode.next = node.next
+  node.next = newnode
+
+  while (head) {
+    console.log(head.data)
+    head = head.next
+  }
+
+  return head
+}
