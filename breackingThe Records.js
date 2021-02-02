4959f1bbd5e54528ba15afdf119e2f07f064a1e0
Daniel Viana <vianadaniel@hotmail.com>
2021-02-02 14:53:05 -0300

    fim

--- breackingThe Records.js
+++ breackingThe Records.js
@@ -5,15 +5,16 @@ function records(s) {
   var countMin = s[0]
   var countMax = s[0]
   for (var i = 1; i < s.length; i++) {
-    if (s[i] < countMin) {
-      countMin = s[i]
-      result[1] += 1
-    } else if (s[i] > countMax) {
+    if (s[i] > countMax) {
       countMax = s[i]
       result[0] += 1
+    } else if (s[i] < countMin) {
+      countMin = s[i]
+      result[1] += 1
     }
   }
 
+  console.log(result)
   return result
 }
 records(scores)
