4450a54a9437fccc11de5579ce4ebc50c5bad791
Daniel Viana <vianadaniel@hotmail.com>
2021-07-01 11:16:57 -0300

    fim

--- pickingNumber.js
+++ pickingNumber.js
@@ -1,20 +1,29 @@
 a =[4, 6, 5, 3, 3, 1]
 
-a.sort((a, b) => a-b)
+
 
 function pick(a){
-    let sum = []
-for(var i=0; i<a.length; i++){
+    
+    // console.log(a.map((e,i,ar) => ar.filter(ee => ((e-ee)==0||(e-ee)==1)).length).reduce((x,y)=>Math.max(x,y)));
+    a.sort((a, b) => b - a);
+    
+    let arr = [];
+    for (let i = 0; i < a.length; i++) {
+        console.log(a[i])
+      let count = 1;
+      for (let j = 0; j < a.length; j++) {
+          console.log(a[j])
+        if (i < j) {
+          if (a[i] - a[j] <= 1) {
+            
+            count++;
+          }
+        }
+      }
+      arr.push(count);
+    }
+    // console.log(arr)
+    return Math.max(...arr)
 
-    if((a[i + 1] - a[i]) <= 1){
-sum.push(a[i])
-    }
-    for(var j=0; j<sum.length; j++){
-if((sum[j+1] - sum[j]) ){
-    console.log(sum[j+1], sum[j])
-}
-    }
-}
-console.log(sum)
 }
 pick(a)
