66f561dbb06bd5543a101500729d0916049a74b3
vianadaniel <vianadaniel@hotmail.com>
2022-05-05 11:57:40 -0300

    codeGenerator

---
+++ codeGeneratorByLenght.js
@@ -0,0 +1,14 @@
+const ar = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+const cod =   `${ar.length + 1}`.length - 1 === 0
+? `C${`0000${ar.length + 1}`}`
+: `C${`0000${ar.length + 1}`.slice(
+      `${ar.length + 1}`.length - 1,
+  )}`
+
+console.log(cod)
+
+
+const codPadStart = `C${`${ar.length + 1}`.padStart(5,0)}`
+
+console.log(codPadStart)
