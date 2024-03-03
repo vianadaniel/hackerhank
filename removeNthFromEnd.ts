fc8ce8c3a08b8a2cd32d02471173c202eaeefca4
Daniel Viana de Almeida <58748927+vianadaniel@users.noreply.github.com>
2024-03-03 08:21:53 -0300

    feat: start

---
+++ removeNthFromEnd.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+    let len: number = 0;
+  let prv: ListNode | null = null;
+  let cur: ListNode | null = head;
+  while (cur) {
+    cur = cur.next;
+    len++;
+  }
+  len -= n;
+  cur = head;
+  while (len) {
+    prv = cur;
+    cur = cur.next;
+    len--;
+  }
+  if (cur === head) head = head.next || null;
+  else prv.next = cur.next;
+  return head;
+};
